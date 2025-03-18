d1cbbc3ab6243b0332f205e8fe6bff75b037ed23
ngelecheikiprop <ngelecheikiprop@gmail.com>
2025-03-18 15:38:42 +0300

    classes in js

---
+++ 0x00-ES6_basic/examples/10-example.js
@@ -0,0 +1,10 @@
+class Car{
+    constructor(name, year){
+        this.name = name;
+        this.year = year;
+    }
+}
+
+const myCar1 = new Car("ford", 2024);
+const myCar2 = new Car("Land rover", 2024);
+console.log(myCar1.name);

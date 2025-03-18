ddabd850cb9d6270a73480eec37ce12fd8b61ffa
ngelecheikiprop <ngelecheikiprop@gmail.com>
2025-03-18 15:33:46 +0300

    examples i forgot to add

---
+++ 0x00-ES6_basic/examples/1-example.js
@@ -0,0 +1,2 @@
+const x = (x,y) => { return x * y };
+console.log(x(3,4));
---
+++ 0x00-ES6_basic/examples/6-example.js
@@ -0,0 +1,4 @@
+const q1 = ["jan", "feb", "march", "april"];
+const q2 = ["may", "june", "july", "august"];
+const year = [...q1, ...q2];
+console.log(year);

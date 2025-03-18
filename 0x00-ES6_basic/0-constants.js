24c440f22f5051bbda0baad2dcbba58a08560387
ngelecheikiprop <ngelecheikiprop@gmail.com>
2025-03-18 14:56:12 +0300

    typo removed

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,9 +1,9 @@
 export function taskFirst(){
-	const task = 'I prefer const when I can';
+	const task = 'I prefer const when I can.';
 	return task;
 }
 export function getLast() {
-	return 'is okay';
+	return ' is okay';
 }
 export function taskNext(){
 	let combination = 'But sometimes let';

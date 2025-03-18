d1e566a273c138c639565b01f6004b8137e0c470
ngelecheikiprop <ngelecheikiprop@gmail.com>
2025-03-18 15:12:53 +0300

    destructuirng

---
+++ 0x00-ES6_basic/examples/4-example.js
@@ -0,0 +1,10 @@
+const person = {
+    firstName : "David",
+    lastName : "Ngelechei",
+    age: 50,
+    eyeColor:"blue"
+};
+
+let {firstName, age } = person;
+console.log(firstName)
+console.log(age)
